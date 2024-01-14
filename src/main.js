b382502fdc855d3a907b54d21aa78cec010298fb
NicDs <nikolasdurango@gmail.com>
2024-01-13 19:39:52 -0500

    added custom styles, created component to navbar/sidebar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/styles/main.css'
+
 createApp(App).mount('#app')
